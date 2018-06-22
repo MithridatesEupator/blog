43c8cb9a6312f1edde8191ce9d3f3e28b2e7bf6a
mithridates++ <fsaulean@gmail.com>
2018-06-21 20:58:13 -0400

    added ability to make posts

--- blog/urls.py
+++ blog/urls.py
@@ -2,11 +2,9 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from django.views.generic import ListView, DetailView
-from .models import Question
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.detail, name='vote'),
+    path('posts/', views.make_post, name='make_post'),
+    path('delete/', views.make_post, name='delete_post'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,52 +1,42 @@
 from django.shortcuts import render, render_to_response, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
-from .models import Choice, Question
+from .models import Post
+from datetime import datetime
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
-    latest_question_list = Question.objects.order_by('-published_date')[:5]
+    blog_post_list = Post.objects.all()[:5]
     context = {
-        'latest_question_list': latest_question_list
+        "blog_post_list": blog_post_list,
     }
     return render(request, 'blog/main.html', context)
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'blog/detail.html', {'question': question})
 
-def vote(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, Choice.DoesNotExist):
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
-    else:
-        selected_choice.votes += 1
-        selected_choice.save()
-        return HttpResponseRedirect(reverse('blog:results', args=(question.id,)))
-    
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'blog/results.html', {'question': question})
+def details(request, id):
+    individual_post = Posts.objects.get(id=id)
+    context = {
+        "individual_post" : individual_post
+    }
+    return render(request, 'blog/detail.html', context)
 
-class IndexView(generic.ListView):
-    template_name = 'blog/main.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        return Question.objects.order_by('-published_date')[:5]
-
-
-class DetailView(generic.DetailView):
-    model = Question
-    template_name = 'blog/detail.html'
-
-
-class ResultsView(generic.DetailView):
-    model = Question
-    template_name = 'blog/results.html'
+def make_post(request):
+    #return render(request, 'blog/upload_post.html')
+    if request.method == 'GET':
+        return render(request, 'blog/upload_post.html', {})
+    elif request.method == 'POST':
+        post = Post(title=request.POST['title'], post_text=request.POST['content'], published_date=timezone.now())
+        post.save()
+        return HttpResponseRedirect(reverse('index'))
+        #return render(request, 'blog/upload_post.html', {})
+        
+def delete_post(request):
+    if request.method == 'GET':
+        return HttpResponseRedirect(reverse('index'))
+    elif request.method == 'POST':
+        delete_id = int(request.POST['delete_id'])
+        post = SomeModel.objects.get(id=delete_id)
+        post.delete()
+        return HttpResponseRedirect(reverse('index'))
